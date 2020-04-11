f61270aabebfe9bc2bd77ba9f80eb897bef26857
Lydia Joynes <lydiajoynesgmail.com>
2020-04-11 18:41:21 +0100

    Beginner Series #3 Sum of Numbers

--- practisecodew.js
+++ practisecodew.js
@@ -88,3 +88,26 @@ function checkCoupon(enteredCode, correctCode, currentDate, expirationDate) {
         return false;
     }
 }
+
+// Beginner Series #3 Sum of Numbers
+
+function getSum(a, b) {
+    let total = 0;
+    if (a === b) {
+        return a;
+    }
+
+    if (a < b) {
+        for (let i = a; i <= b; i++) {
+            total += i;
+        }
+        return total;
+    }
+
+    if (b < a) {
+        for (let i = b; i <= a; i++) {
+            total += i;
+        }
+        return total;
+    }
+}
