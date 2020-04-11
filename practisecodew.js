99c8093f41db0dd8c5b398d50a62dd5c4db55758
Lydia Joynes <lydiajoynesgmail.com>
2020-04-11 18:34:52 +0100

    the coupon code

--- practisecodew.js
+++ practisecodew.js
@@ -76,3 +76,15 @@ function duplicateCount(text) {
     });
     return result;
 }
+
+//the coupon code 
+
+function checkCoupon(enteredCode, correctCode, currentDate, expirationDate) {
+    currentDate = new Date(currentDate);
+    expirationDate = new Date(expirationDate);
+    if (enteredCode === correctCode && currentDate <= expirationDate) {
+        return true;
+    } else {
+        return false;
+    }
+}
