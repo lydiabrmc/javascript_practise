b9b39e157094d4ebcd8d0fc8c48eab2e7fcbbec9
Lydia Joynes <lydiajoynesgmail.com>
2020-04-11 19:54:20 +0100

    count the divisors of a number

--- practisecodew.js
+++ practisecodew.js
@@ -124,3 +124,13 @@ function filter_list(l) {
 function maxMultiple(divisor, bound) {
     return bound - bound % divisor
 }
+
+// count the divisors of a number 
+
+function getDivisorsCnt(n) {
+    for (var d = 0, i = n; i > 0; i--) {
+        if (n % i == 0) d++;
+    }
+
+    return d;
+}
